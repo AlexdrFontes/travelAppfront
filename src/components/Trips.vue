1a9ed5b157306be8c9d3d1f237f505c1b42e87e4
alexandre fontes <alexandrefontes90@gmail.com>
2018-10-11 23:54:44 +0100

    initialized repo

---
+++ src/components/Trips.vue
@@ -0,0 +1,33 @@
+<template>
+ <div id="trips">
+  <div v-for="(trip, index) in trips" :key="trip.id">
+   <p>{{trip.name}} is my {{trip.description}}</p>
+  </div>
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+ data() {
+  return {
+   trips: [],
+   name: [],
+   description: [],
+   errors: []
+  }
+ },
+ created() {
+  axios.get('http://localhost:3000/v1/trips')
+  .then(response => {
+   this.trips = response.data
+  })
+ .catch(e => {
+  this.errors.push(e)
+  })
+ },
+}
+</script>
+
+<style>
+</style>
